1853b1886c45fd7afc045bb49ac85eefd9b7078b
Cihan Dilber <seyyahswiss@gmail.com>
2023-04-24 19:38:37 +0200

    hamcrest added

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,6 +1,7 @@
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;  // bunu hepsine manuel ekliyoruz
+import static org.hamcrest.Matchers.*;       // bunu da
 
 public class ZippoTest {
 
@@ -50,6 +51,30 @@ public class ZippoTest {
                 // bu kisim her zaman calismasina gerek yok. gormek icin
                 .statusCode(200)       // donus kodu 200 mu
                 .contentType(ContentType.JSON)  // donen sonuc JSON mi
+
+        ;
+
+    }
+
+
+    @Test
+    public void checkCountryInResponseBody(){
+
+        given()
+
+                .when()
+                .get("https://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()          // donen body jason datasi. hepsini gostersin istersen    log.all()  yazilir
+                // bu kisim her zaman calismasina gerek yok. gormek icin
+                .statusCode(200)       // donus kodu 200 mu
+                .body("country", equalTo("United States"))  // body nin country degiskeni "United States" esit MI
+
+        // postman de soyle yapmistik
+        // pm.response.json.id --> body.id
+
+
 
         ;
 
