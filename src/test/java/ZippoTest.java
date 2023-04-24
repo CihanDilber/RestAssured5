f1bdeb5df495aba1c75c03a27bc7865fa5eb8c4b
Cihan Dilber <seyyahswiss@gmail.com>
2023-04-24 19:53:18 +0200

    checkStateInResponseBody

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -6,7 +6,7 @@ import static org.hamcrest.Matchers.*;       // bunu da
 public class ZippoTest {
 
     @Test
-    public void test(){
+    public void test() {
 
         given()
                 // hazirlik islemleri : (token, send body, parametreler)
@@ -15,14 +15,14 @@ public class ZippoTest {
                 // endpoint(url), metodu
 
                 .then()
-                // assertion, test, data islemleri
+        // assertion, test, data islemleri
 
         ;
 
     }
 
     @Test
-    public void statusCodeTest(){
+    public void statusCodeTest() {
 
         given()
 
@@ -31,15 +31,15 @@ public class ZippoTest {
 
                 .then()
                 .log().body()          // donen body jason datasi. hepsini gostersin istersen    log.all()  yazilir
-                                       // bu kisim her zaman calismasina gerek yok. gormek icin
+                // bu kisim her zaman calismasina gerek yok. gormek icin
                 .statusCode(200)       // donus kodu 200 mu
 
-                ;
+        ;
 
     }
 
     @Test
-    public void contentTypeTest(){
+    public void contentTypeTest() {
 
         given()
 
@@ -58,7 +58,7 @@ public class ZippoTest {
 
 
     @Test
-    public void checkCountryInResponseBody(){
+    public void checkCountryInResponseBody() {
 
         given()
 
@@ -75,8 +75,47 @@ public class ZippoTest {
         // pm.response.json.id --> body.id
 
 
-
         ;
 
     }
+
+    @Test
+    public void checkStateInResponseBody() {
+
+        given()
+
+                .when()
+                .get("https://api.zippopotam.us/us/90210")
+
+                .then()
+               //  .log().body()
+
+                .statusCode(200)
+                .body("places[0].state", equalTo("California"));
+
+//    PM                            RestAssured
+//    body.country                  body("country")
+//    body.'post code'              body("post code")
+//    body.places[0].'place name'   body("places[0].'place name'")
+//    body.places.'place name'      body("places.'place name'")
+//    bütün place nameleri bir arraylist olarak verir
+//
+//    {
+//        "post code": "90210",
+//            "country": "United States",
+//            "country abbreviation": "US",
+//            "places": [
+//        {
+//            "place name": "Beverly Hills",
+//                "longitude": "-118.4065",
+//                "state": "California",
+//                "state abbreviation": "CA",
+//                "latitude": "34.0901"
+//        }
+//    ]
+    }
+
 }
+
+
+
