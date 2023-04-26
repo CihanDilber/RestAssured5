cfdd9f5a71aac2b237f0374e4b4450b4d2d054e2
Cihan Dilber <seyyahswiss@gmail.com>
2023-04-26 18:37:43 +0200

    task3

--- src/test/java/Tasks.java
+++ src/test/java/Tasks.java
@@ -2,7 +2,8 @@ import Model.ToDo;
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class Tasks {
 
@@ -52,6 +53,33 @@ public class Tasks {
 
         System.out.println("todo = " + todo);
         System.out.println("todo.getTitle() = " + todo.getTitle());
+    }
+
+    /**
+
+     Task 3
+     create a request to https://jsonplaceholder.typicode.com/todos/2
+     expect status 200
+     expect content type JSON
+     expect title in response body to be "quis ut nam facilis et officia qui"
+     */
+
+    @Test
+    public void task3()
+    {
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("title", equalTo("quis ut nam facilis et officia qui"))
+                // hamcrest in faydasi. disari cikartmadan burada assertion yapiliyor.
+        ;
+
     }
 
 }
