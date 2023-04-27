d66062594f49e1f6c49a4d7b16a4a5d4b51716d8
Cihan Dilber <seyyahswiss@gmail.com>
2023-04-27 19:12:33 +0200

    getUsers JsonPath

--- src/test/java/PathAndJsonPath.java
+++ src/test/java/PathAndJsonPath.java
@@ -1,5 +1,10 @@
 
+import GoRest.User;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
@@ -39,5 +44,32 @@ public class PathAndJsonPath {
                 ;
 
         System.out.println("postCode = " + postCode);
+    }
+
+
+    @Test
+    public void getUsers() {
+        Response response =
+                given()
+
+                        .when()
+                        .get("https://gorest.co.in/public/v2/users")
+
+                        .then()
+                        //.log().body()
+                        .extract().response();
+        ;
+
+        int idPath = response.path("[2].id");
+        int idJsonPath = response.jsonPath().getInt("[2].id");
+
+        System.out.println("idPath = " + idPath);
+        System.out.println("idJsonPath = " + idJsonPath);
+
+        User[] usersPath= response.as(User[].class);
+        List<User> usersJsonPath= response.jsonPath().getList("", User.class);
+
+        System.out.println("usersPath = " + Arrays.toString(usersPath));
+        System.out.println("usersJsonPath = " + usersJsonPath);
     }
 }
